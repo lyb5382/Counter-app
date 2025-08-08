de6963df4ffc31570105988a6d073c6daef83f63
lyb5382 <lyb538202@gmail.com>
2025-08-08 10:18:08 +0900

    f

---
+++ src/components/Controller.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Controller = ({ onClickButton }) => {
+    const buttonValues = [-100, -10, -1, 1, 10, 100]
+    return (
+        <div>
+            {buttonValues.map((btn, i) =>
+                <button onClick={onClickButton(btn)} key={i}>{btn > 0 ? `+${btn}` : btn}</button>
+            )}
+        </div>
+    )
+}
+
+export default Controller
